288bcf38da301947cef1fc5ed1e1fcbb566429db
rishitsheth1 <rsheth618@gmail.com>
2019-05-28 18:11:24 -0400

    .

--- Assignment 3/SahilMahajan_Comp308_Lab3/SahilMahajan_Comp308_Lab3/README.md
+++
@@ -1,3 +0,0 @@
-﻿ # CRUDAngular2Test
-
-
--- Assignment 3/SahilMahajan_Comp308_Lab3/SahilMahajan_Comp308_Lab3/public/app/articles/articles.component.ts
+++
@@ -1,8 +0,0 @@
-﻿import { Component } from '@angular/core';
-import { ArticlesService } from './articles.service';
-@Component({
-    selector: 'articles',
-    template: '<router-outlet></router-outlet>',
-    providers: [ArticlesService]
-})
-export class ArticlesComponent { }
